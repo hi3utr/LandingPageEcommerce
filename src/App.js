d69556e0fd866d65cdcdb79406b800180f73c868
HieuPC <hi3utr@email.com>
2022-08-23 23:05:08 +0700

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import"../src/components/miniBar/MiniBar"
+import MiniBar from "../src/components/miniBar/MiniBar";
+function App() {
+  return (
+    <div className="font-raleway h-screen ">
+      <MiniBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/miniBar/MiniBar.jsx
@@ -0,0 +1,11 @@
+const MiniBar = () => {
+    return ( 
+        <div className="bg-[#F1F2F3] text-center flex justify-center items-center">
+            <p className=" py-[12px] text-xs  ">NEW COLLECTION 2022 </p>
+            <p className="px-[9px]">|</p>
+            <a href="" className=" py-[12px] text-sm underline decoration-1  ">SHOP NOW</a>
+        </div>
+     );
+}
+ 
+export default MiniBar;
